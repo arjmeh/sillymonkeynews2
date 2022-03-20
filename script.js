fc4c4743b19555c62ab385bf0119a678286f9dbb
arjmeh <arjunisawesome101@gmail.com>
2022-03-19 19:45:48 -0700

    initialcommit

---
+++ script.js
@@ -0,0 +1,6 @@
+var clicktoshow = document.getElementById("yuckpic")
+var showbutton = document.getElementById("showbutton")
+function showpic() {
+    clicktoshow.style.display = "block";
+    showbutton.style.display = "none";
+}
